2c7c7018a796c27c6d6b29d2deb9c6bc357d9ed9
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-28 22:04:31 -0500

    Add products data to products.js

---
+++ data/products.js
@@ -0,0 +1,60 @@
+export const products = [
+
+{
+id:"N2XY",
+
+application:["Distribución","Bombeo"],
+
+installation:[
+"Enterrado",
+"Bandeja"
+],
+
+voltage:["1kV"],
+
+temperature:"90°C",
+
+features:[
+"UV"
+],
+
+name:"N2XY",
+
+description:
+"Cable XLPE BT",
+
+brochure:
+"https://ec.prysmian.com",
+
+score:100
+},
+
+{
+id:"SUBMERSIBLE",
+
+application:["Bombeo"],
+
+installation:[
+"Sumergido"
+],
+
+voltage:["600V"],
+
+temperature:"90°C",
+
+features:[
+"Resistente Agua"
+],
+
+name:"Cable Bomba Sumergible",
+
+description:
+"Bombas sumergibles",
+
+brochure:
+"https://ec.prysmian.com",
+
+score:100
+}
+
+];
