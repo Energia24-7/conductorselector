b65e88b0b276b63e1e7be6346ee84d9b32041549
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-28 22:03:50 -0500

    Add criteria.js with installation, voltage, and temperature data

---
+++ data/criteria.js
@@ -0,0 +1,26 @@
+export const criteria = {
+
+installations:[
+"Enterrado",
+"Bandeja",
+"Tubería",
+"Aéreo",
+"Exterior",
+"Sumergido"
+],
+
+voltages:[
+"600V",
+"1kV",
+"5kV",
+"15kV",
+"25kV",
+"35kV"
+],
+
+temperatures:[
+"75°C",
+"90°C"
+]
+
+};
