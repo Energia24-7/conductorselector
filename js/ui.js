b146f59af05170b4067b4889de8b131f1d4675f3
Energia24-7 <fastindustrialsupply@gmail.com>
2026-06-28 22:28:05 -0500

    Refactor renderResults function for better readability

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,35 @@
 export function renderResults(results){
 
-const container=
-document.getElementById('results');
+const container =
+document.getElementById(
+'results'
+);
 
 container.innerHTML='';
 
+if(results.length===0){
+
+container.innerHTML=
+'<h3>No se encontraron resultados</h3>';
+
+return;
+}
+
 results.forEach(item=>{
 
-container.innerHTML+=`
+const p =
+item.product;
+
+container.innerHTML += `
 
 <div class="result-card">
 
 <h3>
-${item.product.name}
+${p.name}
 </h3>
 
 <p>
-${item.product.description}
+${p.description}
 </p>
 
 <p class="score">
@@ -25,10 +38,26 @@ ${item.score}
 %
 </p>
 
+<div class="tags">
+
+<span class="tag">
+${p.voltageClass}
+</span>
+
+<span class="tag">
+${p.conductor}
+</span>
+
+<span class="tag">
+${p.insulation}
+</span>
+
+</div>
+
 <a
 class="brochure"
 target="_blank"
-href="${item.product.brochure}">
+href="${p.brochure}">
 Ver Folleto
 </a>
 
